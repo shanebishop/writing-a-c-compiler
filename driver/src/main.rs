0ae19dd84fed383fc22885e64ad571cd9ac54a78
Shane Bishop <shane.bishop@outlook.com>
2025-08-07 07:32:54 -0400

    Add argument parsing

--- driver/src/main.rs
+++ driver/src/main.rs
@@ -1,3 +1,27 @@
+use clap::Parser;
+
+/// Clap program arguments
+#[derive(Parser, Debug)]
+#[command(about = "A C compiler", long_about = None)]
+struct Args {
+    /// Path to C source file to compile
+    source_path: String,
+
+    /// Run the lexer, but stop before parsing
+    #[arg(short, long)]
+    lex: bool,
+
+    /// Run the lexer and parser, but stop before
+    /// assembly generation
+    #[arg(short, long)]
+    parse: bool,
+
+    /// Perform lexing, parsing, and assembly
+    /// generation, but stop before code emission
+    #[arg(short, long)]
+    codegen: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
 }
